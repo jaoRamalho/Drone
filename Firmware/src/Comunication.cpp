f7a74722552e5e8c12c68e6256ed40a711b2da04
unknown <pedropichelli04@gmail.com>
2025-11-03 18:20:26 -0300

    Pousos automáticos em situação de perda de conexão ou bateria baixa

--- Firmware/src/Comunication.cpp
+++ Firmware/src/Comunication.cpp
@@ -1,5 +1,8 @@
 #include "Comunication.hpp"
 
+#define MIN_SAFE_BATTERY 20     // % → inicia pouso automático
+#define CONNECTION_TIMEOUT 3000 // ms sem sinal
+
 RF24 radio(CE_PIN, CSN_PIN);
 
 // Endereços (podem ser parametrizados depois)
@@ -8,6 +11,8 @@ const uint8_t rxAddress[6] = "00002"; // drone recebe/controle recebe telemetria
 
 volatile bool sendPingFlag = false;
 
+unsigned long now = 0;
+unsigned long lastCommandReceivedMillis = 0;
 unsigned long lastCommandMillis = 0;
 const unsigned long COMMAND_INTERVAL = 5000UL; // 5 segundos
 
@@ -222,6 +227,13 @@ void Communication::receiveCommand()
         uint8_t cmd[5];
         radio.read(cmd, sizeof(cmd));
 
+        if (lastCommandReceivedMillis != 0) {
+            now = millis();
+            if (now - lastCommandReceivedMillis > CONNECTION_TIMEOUT) {
+                Serial.println("[EMERGÊNCIA] Sinal perdido - iniciando pouso seguro!");
+                // função para pouso seguro
+            }
+        }
         // Verifica checksum
         if (random(0, 100) < 5) { // 5% de chance de introduzir erro para testes
             uint8_t byteIdx = random(0, 5); // escolhe um dos 5 bytes
@@ -255,6 +267,7 @@ void Communication::receiveCommand()
             action = receivedAction;
             power = receivedPower;
             lastReceivedSeq = seq;
+            lastCommandReceivedMillis = millis(); // Atualiza o tempo da última comunicação
 
             Serial.print("Ação = ");
             Serial.print(action);
@@ -265,6 +278,10 @@ void Communication::receiveCommand()
             // Aqui você aplicaria o comando ao drone ou enfileiraria para processamento
         }
 
+        if (battery <= MIN_SAFE_BATTERY) {
+            Serial.println("[AVISO] Bateria baixa - pouso automático!");
+            // função para pouso seguro
+        }
         // Prepara telemetria para envio: seq_hi, seq_lo, battery, alt_hi, alt_lo, statusFlags, checksum
         uint8_t ackPayload[7];
         ackPayload[0] = (uint8_t)((seq >> 8) & 0xFF);
